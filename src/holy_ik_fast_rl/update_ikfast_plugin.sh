0b9df978c3a0e68cd81a637e27d3e4f5aece23d8
Simon Bilgeri <Simon.Bilgeri@tum.de>
2015-06-11 16:46:41 +0200

    recompiled right leg ikfast solver

--- src/holy_ik_fast_rl/update_ikfast_plugin.sh
+++ src/holy_ik_fast_rl/update_ikfast_plugin.sh
@@ -1,1 +1,1 @@
-rosrun moveit_ikfast create_ikfast_moveit_plugin.py awesome-o RightLeg holy_ik_fast /home/simon/ROS_HO/src/holy_ik_fast/src/awesome-o_RightLeg_ikfast_solver.cpp
+rosrun moveit_ikfast create_ikfast_moveit_plugin.py awesome-o RightLeg holy_ik_fast_rl /home/simon/ROS_HO/src/holy_ik_fast_rl/src/awesome-o_RightLeg_ikfast_solver.cpp
