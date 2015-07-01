2552bf0268833855c6c2955f1da53cc34acf53fe
SimBil91 <Simon.Bilgeri@gmx.de>
2015-07-01 21:01:11 +0200

    kinect arms work! And added rosbags

--- src/holy/src/kinect.cpp
+++ src/holy/src/kinect.cpp
@@ -34,8 +34,8 @@ void Kinect::updateTF()
     // Left right is swapped in Kinect frame!
     kin_listener->lookupTransform("tracker/user_1/torso", "tracker/user_1/left_foot", ros::Time(0), kin_transforms[Core::Limb::LEFT_FOOT]);
     kin_listener->lookupTransform("tracker/user_1/torso", "tracker/user_1/right_foot", ros::Time(0), kin_transforms[Core::Limb::RIGHT_FOOT]);
-    kin_listener->lookupTransform("tracker/user_1/torso", "tracker/user_1/left_hand", ros::Time(0), kin_transforms[Core::Limb::LEFT_HAND]);
-    kin_listener->lookupTransform("tracker/user_1/torso", "tracker/user_1/right_hand", ros::Time(0), kin_transforms[Core::Limb::RIGHT_HAND]);
+    kin_listener->lookupTransform("tracker/user_1/left_shoulder", "tracker/user_1/left_hand", ros::Time(0), kin_transforms[Core::Limb::LEFT_HAND]);
+    kin_listener->lookupTransform("tracker/user_1/right_shoulder", "tracker/user_1/right_hand", ros::Time(0), kin_transforms[Core::Limb::RIGHT_HAND]);
 }
 
 double Kinect::compute_norm(tf::StampedTransform &frame1) {
@@ -46,16 +46,29 @@ void Kinect::human_to_robopose()
 {
     // Arms
     // Get orientation from Kinect
-    double alroll,alpitch=0,alyaw=0;
-    alroll= acos(kin_transforms[Core::Limb::LEFT_HAND].getOrigin().x()/compute_norm(kin_transforms[Core::Limb::LEFT_HAND]));
-    alyaw= 0;//acos(kin_transforms[Core::Limb::LEFT_HAND].getOrigin().z()/compute_norm(kin_transforms[Core::Limb::LEFT_HAND]));
-    alpitch= acos(-kin_transforms[Core::Limb::LEFT_HAND].getOrigin().z()/compute_norm(kin_transforms[Core::Limb::LEFT_HAND]));
+    double alroll,alpitch=0,alyaw=0,lnorm;
+    double arroll,arpitch=0,aryaw=0,rnorm;
+    lnorm=compute_norm(kin_transforms[Core::Limb::LEFT_HAND]);
+    rnorm=compute_norm(kin_transforms[Core::Limb::RIGHT_HAND]);
+    alroll= acos(kin_transforms[Core::Limb::LEFT_HAND].getOrigin().z()/lnorm) -M_PI/2;
+    arroll= acos(kin_transforms[Core::Limb::RIGHT_HAND].getOrigin().z()/rnorm) -M_PI/2;
+    //alroll=Poses::d2r(30)-1*alroll;
+    alyaw= 0;//acos(kin_transforms[Core::Limb::LEFT_HAND].getOrigin().z());
+    alpitch= acos(kin_transforms[Core::Limb::LEFT_HAND].getOrigin().x()/lnorm) -M_PI/2;
+    arpitch= -(acos(-kin_transforms[Core::Limb::RIGHT_HAND].getOrigin().x()/rnorm) -M_PI/2);
+    if (kin_transforms[Core::Limb::LEFT_HAND].getOrigin().y()>0) {
+        alpitch= M_PI/2 + (M_PI/2 -alpitch);
+    }
+    if (kin_transforms[Core::Limb::RIGHT_HAND].getOrigin().y()>0) {
+        arpitch= -M_PI/2 + (-M_PI/2 -arpitch);
+    }
     //ROS_INFO("%lf,%lf,%lf",kin_transforms[Core::Limb::LEFT_HAND].getOrigin().x(),kin_transforms[Core::Limb::LEFT_HAND].getOrigin().y(),kin_transforms[Core::Limb::LEFT_HAND].getOrigin().z());
-    ROS_INFO("%lf,%lf,%lf",Poses::r2d(alroll),Poses::r2d(alpitch),Poses::r2d(alyaw));
+    ROS_INFO("%lf,%lf,%lf",Poses::r2d(arroll),Poses::r2d(arpitch),Poses::r2d(aryaw));
+    //ROS_INFO("%lf,%lf,%lf",alroll,alpitch,alyaw);
     pose_from_kinect = RoboPose (std::vector<LimbPose> {
                            //changed to have lower arms
-                                           LimbPose(Core::Limb::LEFT_HAND,  alroll, alpitch+Poses::d2r(90), alyaw, 0, 0, 0),
-                                           LimbPose(Core::Limb::RIGHT_HAND, 0,  0, 0,  0, 0, 0),
+                                           LimbPose(Core::Limb::LEFT_HAND,  alroll , alpitch, alyaw, 0, 0, 0),
+                                           LimbPose(Core::Limb::RIGHT_HAND, arroll,  arpitch, 0,  0, 0, 0),
                                        } , "pose_kinect");
 }
 
